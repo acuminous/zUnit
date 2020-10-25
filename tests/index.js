bea1b5d09fea6830cb4b04420fa2c57021ffb83d
Stephen Cresswell <git@stephen-cresswell.net>
2020-10-25 12:37:47 +0000

    Fixed colours in CI mode

--- tests/index.js
+++ tests/index.js
@@ -4,8 +4,10 @@ const { MultiReporter, ConsoleReporter, TapReporter } = require('..');
 const filename = path.resolve(__dirname, process.argv[2]);
 const runnable = require(filename);
 
+const interactive = String(process.env.CI).toLowerCase() !== 'true';
+
 const reporter = new MultiReporter()
-  .add(new ConsoleReporter())
+  .add(new ConsoleReporter({ colours: interactive }))
   .add(new TapReporter());
 
 runnable.run(reporter).then(() => {
