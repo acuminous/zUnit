760629de5ec56b7dd010e26bcbb3f309c18473d4
Stephen Cresswell <git@stephen-cresswell.net>
2020-10-26 22:40:59 +0000

    Add RunnableEvents and RunnableOutcomes to the zUnit API

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const Suite = require('./lib/Suite');
 const Test = require('./lib/Test');
+const RunnableEvents = require('./lib/RunnableEvents');
+const RunnableOutcomes = require('./lib/RunnableOutcomes');
 const Syntax = require('./lib/Syntax');
 const reporters = require('./lib/reporters');
 
 module.exports = {
   Suite,
   Test,
+  RunnableEvents,
+  RunnableOutcomes,
   ...Syntax,
   ...reporters,
 }
