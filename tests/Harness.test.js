2f212fd91da1ad2e09f9cfa6949b727ca27e90fc
Stephen Cresswell <git@stephen-cresswell.net>
2020-11-07 09:23:33 +0000

    Improve test coverage

--- tests/Harness.test.js
+++ tests/Harness.test.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
-const { describe, NullReporter, Harness, Suite, Test } = require('..');
+const { describe, GraphReporter, NullReporter, Harness, Suite, Test, RunnableOutcomes } = require('..');
 
 describe('Harnesses', ({ it }) => {
 
   const reporter = new NullReporter();
 
   it('should run a test suite', async () => {
-    const test1 = new Test('should run test 1', pass);
-    const test2 = new Test('should run test 2', pass);
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', pass);
     const suite = new Suite('Test Suite').add(test1, test2);
     const harness = new Harness().set(suite);
 
@@ -17,17 +17,129 @@ describe('Harnesses', ({ it }) => {
   });
 
   it('should run an individual test', async () => {
-    const test = new Test('should run test', pass);
+    const test = new Test('Test', pass);
     const harness = new Harness().set(test);
 
     await harness.run(reporter);
 
     assert.equal(harness.passed, true);
   });
+
+  it('should skip a single test in a nested suite', async () => {
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('should run test 2', pass, { skip: true });
+    const test3 = new Test('should run test 3', pass);
+    const child1 = new Suite('Child 1').add(test1, test2);
+    const child2 = new Suite('Child 2').add(test3);
+    const parent = new Suite('Parent').add(child1, child2);
+    const harness = new Harness().set(parent);
+
+    const reporter = new GraphReporter();
+    await harness.run(reporter);
+
+    const graph = reporter.toGraph();
+    assert.equal(graph.result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0, 0).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0, 1).result, RunnableOutcomes.SKIPPED);
+    assert.equal(graph.resolve(1).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(1, 0).result, RunnableOutcomes.PASSED);
+  });
+
+  it('should skip an entire suite in a nested suite', async () => {
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', pass);
+    const test3 = new Test('Test 3', pass);
+    const child1 = new Suite('Child 1', { skip: true }).add(test1, test2);
+    const child2 = new Suite('Child 2').add(test3);
+    const parent = new Suite('Parent').add(child1, child2);
+    const harness = new Harness().set(parent);
+
+    const reporter = new GraphReporter();
+    await harness.run(reporter);
+
+    const graph = reporter.toGraph();
+    assert.equal(graph.result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0).result, RunnableOutcomes.SKIPPED);
+    assert.equal(graph.resolve(0, 0).result, RunnableOutcomes.SKIPPED);
+    assert.equal(graph.resolve(0, 1).result, RunnableOutcomes.SKIPPED);
+    assert.equal(graph.resolve(1).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(1, 0).result, RunnableOutcomes.PASSED);
+  });
+
+  it('should only run exclusive tests in a nested suite', async () => {
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', pass, { exclusive: true });
+    const test3 = new Test('Test 3', pass, { exclusive: true });
+    const test4 = new Test('Test 4', pass, { exclusive: true });
+    const child1 = new Suite('Child 1').add(test1, test2, test3);
+    const child2 = new Suite('Child 2').add(test4);
+    const parent = new Suite('Parent').add(child1, child2);
+    const harness = new Harness().set(parent);
+
+    const reporter = new GraphReporter();
+    await harness.run(reporter);
+
+    const graph = reporter.toGraph();
+    assert.equal(graph.result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0, 0).name, 'Test 2');
+    assert.equal(graph.resolve(0, 0).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0, 1).name, 'Test 3');
+    assert.equal(graph.resolve(0, 1).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(1).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(1, 0).name, 'Test 4');
+    assert.equal(graph.resolve(1, 0).result, RunnableOutcomes.PASSED);
+  });
+
+  it('should only run exclusive suites in a nested suite', async () => {
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', pass);
+    const test3 = new Test('Test 3', pass);
+    const child1 = new Suite('Child 1', { exclusive: true }).add(test1, test2);
+    const child2 = new Suite('Child 2').add(test3);
+    const parent = new Suite('Parent').add(child1, child2);
+    const harness = new Harness().set(parent);
+
+    const reporter = new GraphReporter();
+    await harness.run(reporter);
+
+    const graph = reporter.toGraph();
+    assert.equal(graph.result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0, 0).name, 'Test 1');
+    assert.equal(graph.resolve(0, 0).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0, 1).name, 'Test 2');
+    assert.equal(graph.resolve(0, 1).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(1), undefined);
+  });
+
+
+  it('should skip an entire suite in a nested suite, even when exclusive', async () => {
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', pass);
+    const test3 = new Test('Test 3', pass);
+    const child1 = new Suite('Child 1', { skip: true, exclusive: true }).add(test1, test2);
+    const child2 = new Suite('Child 2').add(test3);
+    const parent = new Suite('Parent').add(child1, child2);
+    const harness = new Harness().set(parent);
+
+    const reporter = new GraphReporter();
+    await harness.run(reporter);
+
+    const graph = reporter.toGraph();
+    assert.equal(graph.result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0).result, RunnableOutcomes.SKIPPED);
+    assert.equal(graph.resolve(0, 0).result, RunnableOutcomes.SKIPPED);
+    assert.equal(graph.resolve(0, 1).result, RunnableOutcomes.SKIPPED);
+    assert.equal(graph.resolve(1).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(1, 0).result, RunnableOutcomes.PASSED);
+  });
 });
 
 function pass() {
   return Promise.resolve();
 }
+
 
 
--- tests/Suite.test.js
+++ tests/Suite.test.js
@@ -1,13 +1,13 @@
 const assert = require('assert');
-const { describe, NullReporter, Suite, Test } = require('..');
+const { describe, GraphReporter, NullReporter, Suite, Test, RunnableOutcomes } = require('..');
 
 describe('Suites', ({ it }) => {
 
   const reporter = new NullReporter();
 
   it('should report successful tests', async () => {
-    const test1 = new Test('should run test 1', pass);
-    const test2 = new Test('should run test 2', pass);
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', pass);
     const suite = new Suite('Test Suite').add(test1, test2);
 
     await suite.run(reporter);
@@ -18,8 +18,8 @@ describe('Suites', ({ it }) => {
   });
 
   it('should report failing tests', async () => {
-    const test1 = new Test('should run test 1', pass);
-    const test2 = new Test('should run test 2', fail);
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', fail);
     const suite = new Suite('Test Suite').add(test1, test2);
 
     await suite.run(reporter);
@@ -29,9 +29,9 @@ describe('Suites', ({ it }) => {
     assert.equal(test2.failed, true);
   });
 
-  it('should skip tests (runner)', async () => {
-    const test1 = new Test('should run test 1', pass);
-    const test2 = new Test('should run test 2', fail);
+  it('should skip all tests in the test run', async () => {
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', fail);
     const suite = new Suite('Test Suite').add(test1, test2);
 
     await suite.run(reporter, { skip: true });
@@ -41,9 +41,9 @@ describe('Suites', ({ it }) => {
     assert.equal(test2.skipped, true);
   });
 
-  it('should skip tests (configuration)', async () => {
-    const test1 = new Test('should run test 1', pass);
-    const test2 = new Test('should run test 2', fail);
+  it('should skip the entire test suite', async () => {
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', fail);
     const suite = new Suite('Test Suite', { skip: true }).add(test1, test2);
 
     await suite.run(reporter);
@@ -53,9 +53,9 @@ describe('Suites', ({ it }) => {
     assert.equal(test2.skipped, true);
   });
 
-  it('should skip tests (individual)', async () => {
-    const test1 = new Test('should run test 1', pass);
-    const test2 = new Test('should run test 2', pass, { skip: true });
+  it('should skip individual tests', async () => {
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', pass, { skip: true });
     const suite = new Suite('Test Suite').add(test1, test2);
 
     await suite.run(reporter);
@@ -66,8 +66,8 @@ describe('Suites', ({ it }) => {
   });
 
   it('should abort early (runner)', async () => {
-    const test1 = new Test('should run test 1', fail);
-    const test2 = new Test('should run test 2', pass);
+    const test1 = new Test('Test 1', fail);
+    const test2 = new Test('Test 2', pass);
     const suite = new Suite('Test Suite').add(test1, test2);
 
     await suite.run(reporter, { abort: true });
@@ -78,8 +78,8 @@ describe('Suites', ({ it }) => {
   });
 
   it('should aborting early (configuration)', async () => {
-    const test1 = new Test('should run test 1', fail);
-    const test2 = new Test('should run test 2', pass);
+    const test1 = new Test('Test 1', fail);
+    const test2 = new Test('Test 2', pass);
     const suite = new Suite('Test Suite', { abort: true }).add(test1, test2);
 
     await suite.run(reporter);
@@ -90,8 +90,8 @@ describe('Suites', ({ it }) => {
   });
 
   it('should only run exclusive tests (test configuration)', async () => {
-    const test1 = new Test('should not run test 1', pass);
-    const test2 = new Test('should run test 2', pass, { exclusive: true });
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', pass, { exclusive: true });
     const suite = new Suite('Test Suite').add(test1, test2);
 
     await suite.run(reporter, {}, false);
@@ -131,6 +131,30 @@ describe('Suites', ({ it }) => {
     assert.equal(parent.stats.skipped, 0);
   });
 
+  it('should skip exclusive tests', async () => {
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', pass, { skip: true, exclusive: true });
+    const suite = new Suite('Test Suite').add(test1, test2);
+
+    await suite.run(reporter);
+
+    assert.equal(suite.passed, true);
+    assert.equal(test1.passed, true);
+    assert.equal(test2.skipped, true);
+  });
+
+  it('should skip the exclusive test suites', async () => {
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', fail);
+    const suite = new Suite('Test Suite', { skip: true, exclusive: true }).add(test1, test2);
+
+    await suite.run(reporter);
+
+    assert.equal(suite.skipped, true);
+    assert.equal(test1.skipped, true);
+    assert.equal(test2.skipped, true);
+  });
+
   it('should support nesting', async () => {
     const test1 = new Test('Test 1', pass);
     const test2 = new Test('Test 2', fail);
@@ -168,13 +192,14 @@ describe('Suites', ({ it }) => {
   });
 
   it('should finalise a suite of tests', async () => {
-    const test1 = new Test('should run test 1', pass);
-    const test2 = new Test('should run test 2', pass);
-    const test3 = new Test('should run test 3', pass);
+    const test1 = new Test('Test 1', pass);
+    const test2 = new Test('Test 2', pass);
+    const test3 = new Test('Test 3', pass);
     const child1 = new Suite('Child 1').add(test1, test2);
     const child2 = new Suite('Child 2').add(test3);
     const parent = new Suite('Parent').add(child1, child2);
 
+    const reporter = new GraphReporter();
     const finalised = parent.finalise();
     await finalised.run(reporter);
 
@@ -188,43 +213,18 @@ describe('Suites', ({ it }) => {
     assert.equal(test3.passed, false);
     assert.equal(test3.number, undefined);
 
-    assert.equal(finalised.name, 'Parent');
-    assert.equal(finalised.passed, true);
-    assert.equal(finalised._runnables[0]._runnables[0].name, 'should run test 1');
-    assert.equal(finalised._runnables[0]._runnables[0].number, 1);
-    assert.equal(finalised._runnables[0]._runnables[1].passed, true);
-    assert.equal(finalised._runnables[0]._runnables[1].name, 'should run test 2');
-    assert.equal(finalised._runnables[0]._runnables[1].number, 2);
-    assert.equal(finalised._runnables[0]._runnables[0].passed, true);
-    assert.equal(finalised._runnables[1]._runnables[0].name, 'should run test 3');
-    assert.equal(finalised._runnables[1]._runnables[0].number, 3);
-    assert.equal(finalised._runnables[1]._runnables[0].passed, true);
-    assert.equal(finalised.nextNumber, 4);
-  });
-
-  it('should finalise a nested suite of tests', async () => {
-    const test1 = new Test('should run test 1', pass);
-    const test2 = new Test('should run test 2', pass);
-    const suite = new Suite('Test Suite').add(test1, test2);
-
-    const finalised = suite.finalise();
-    await finalised.run(reporter);
-
-    assert.equal(suite.passed, false);
-    assert.equal(test1.passed, false);
-    assert.equal(test1.number, undefined);
-    assert.equal(test2.passed, false);
-    assert.equal(test2.number, undefined);
-
-    assert.equal(finalised.name, 'Test Suite');
-    assert.equal(finalised.passed, true);
-    assert.equal(finalised._runnables[0].name, 'should run test 1');
-    assert.equal(finalised._runnables[0].number, 1);
-    assert.equal(finalised._runnables[0].passed, true);
-    assert.equal(finalised._runnables[1].name, 'should run test 2');
-    assert.equal(finalised._runnables[1].number, 2);
-    assert.equal(finalised._runnables[1].passed, true);
-    assert.equal(finalised.nextNumber, 3);
+    const graph = reporter.toGraph();
+    assert.equal(graph.name, 'Parent');
+    assert.equal(graph.result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0, 0).name, 'Test 1');
+    assert.equal(graph.resolve(0, 0).number, 1);
+    assert.equal(graph.resolve(0, 1).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(0, 1).name, 'Test 2');
+    assert.equal(graph.resolve(0, 1).number, 2);
+    assert.equal(graph.resolve(0, 0).result, RunnableOutcomes.PASSED);
+    assert.equal(graph.resolve(1, 0).name, 'Test 3');
+    assert.equal(graph.resolve(1, 0).number, 3);
+    assert.equal(graph.resolve(1, 0).result, RunnableOutcomes.PASSED);
   });
 });
 
