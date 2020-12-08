38d8a3b97e97300b490b793ad465b1495351ebdf
Stephen Cresswell <git@stephen-cresswell.net>
2020-12-08 10:42:48 +0000

    Linting

--- tests/support/polyfill.js
+++ tests/support/polyfill.js
@@ -4,5 +4,5 @@ const assert = require('assert');
 if (!assert.match) {
   assert.match = function match(actual, regexp) {
     assert.ok(regexp.test(actual), `The input did not match the regular expression ${regexp}. Input:${EOL}${actual}`);
-  }
+  };
 }
