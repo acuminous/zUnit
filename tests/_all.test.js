69413bbdef5a74e516430c2c60fa2a933906a77e
Stephen Cresswell <git@stephen-cresswell.net>
2020-12-30 00:58:43 +0000

    Remove redundant test suite

--- tests/_all.test.js
+++ tests/_all.test.js
@@ -1,9 +1,8 @@
-const harnessTests = require('./Harness.test');
 const suiteTests = require('./Suite.test');
 const testTests = require('./Test.test');
 const hookTests = require('./Hook.test');
 const reporterTests = require('./reporters/_reporter.test');
 
 describe('zUnit', () => {
-  include(harnessTests, suiteTests, testTests, hookTests, reporterTests);
+  include(suiteTests, testTests, hookTests, reporterTests);
 });
