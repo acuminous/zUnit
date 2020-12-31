d6f70eafd5e5475d7be41c8fb40972823e11d6f3
Stephen Cresswell <git@stephen-cresswell.net>
2020-12-31 08:37:38 +0000

    Run the example project from tests

---
+++ test/example.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const path = require('path');
+const { Harness, Suite } = require('..');
+
+describe('Example', () => {
+
+  it('should run the example project', async () => {
+    const directory = path.join(process.cwd(), 'example', 'test');
+    const suite = new Suite('Example').discover({ directory });
+    const harness = new Harness(suite);
+
+    const report = await harness.run();
+
+    assert.stats(report.stats, { tests: 28, passed: 11, failed: 1, skipped: 16 });
+  });
+
+})
