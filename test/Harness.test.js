16a238c2f488e033e5fb11818d5d804b680c2a50
Stephen Cresswell <git@stephen-cresswell.net>
2020-12-31 09:46:56 +0000

    Add harness tests

---
+++ test/Harness.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { run, pass, fail, skip, timeout } = require('./support/helpers');
+const { Harness, Test, GraphReporter } = require('..');
+
+describe('Harness', () => {
+
+  describe('Run', () => {
+
+    it('should error if not initialised with a testable', async () => {
+      const harness = new Harness();
+      const messagePattern = /The harness must be initialised with a suite or test/;
+      await assert.rejects(async () => harness.run(), messagePattern);
+      await assert.rejects(async () => harness.run({}), messagePattern);
+    }, { exclusive: true });
+  });
+
+});
+
