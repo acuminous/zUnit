ee58fd1adaade942c82dfdf3c94420e7757a5771
Stephen Cresswell <229672+cressie176@users.noreply.github.com>
2021-01-27 08:12:35 +0000

    Beef up syntax tests

--- test/syntax.test.js
+++ test/syntax.test.js
@@ -1,19 +1,46 @@
 const assert = require('assert');
-const { Harness, Suite, Test, syntax } = require('..');
+const { Harness, Suite, Test, before, beforeEach, describe, it } = require('..');
 
-module.exports = new Suite('Syntax')
+const hooksSuite = new Suite('Hooks')
+  .add(new Test('should name a hooks', async () => {
+    const names = [];
+    const suite = describe('Suite', () => {
+      before('one', (h) => {
+        names.push(h.name);
+      });
+      beforeEach('two', (h) => {
+        names.push(h.name);
+      });
+      beforeEach('three', (h) => {
+        names.push(h.name);
+      });
+      afterEach('four', (h) => {
+        names.push(h.name);
+      });
+      afterEach('five', (h) => {
+        names.push(h.name);
+      });
+      after('six', (h) => {
+        names.push(h.name);
+      });
 
+      it('Test', () => {});
+    });
+
+    const harness = new Harness(suite);
+
+    await harness.run();
+    assert.deepStrictEqual(names, ['one', 'two', 'three', 'four', 'five', 'six']);
+  }))
   .add(new Test('should configure before hooks with a timeout', async () => {
-    const suite = syntax.describe('h1', () => {
-      syntax.before(async () => {
+    const suite = describe('Suite', () => {
+      before(async () => {
         return new Promise((resolve) => {
           setTimeout(resolve, 200);
         });
       }, { timeout: 100 });
 
-      syntax.it('t1', () => {
-        assert.fail('Should not have run');
-      });
+      it('Test', () => {});
     });
 
     const harness = new Harness(suite);
@@ -22,20 +49,17 @@ module.exports = new Suite('Syntax')
     assert.ok(report.failed);
     assert.ok(report.incomplete);
     assert.stats(report.stats, { tests: 1 });
-    assert.match(report.errors[0].message, /Timed out after 100ms/);    
+    assert.match(report.errors[0].message, /Timed out after 100ms/);
   }))
-
   .add(new Test('should configure beforeEach hooks with a timeout', async () => {
-    const suite = syntax.describe('h1', () => {
-      syntax.beforeEach(async () => {
+    const suite = describe('Suite', () => {
+      beforeEach(async () => {
         return new Promise((resolve) => {
           setTimeout(resolve, 200);
         });
       }, { timeout: 100 });
 
-      syntax.it('t1', () => {
-        assert.fail('Should not have run');
-      });
+      it('Test', () => {});
     });
 
     const harness = new Harness(suite);
@@ -44,5 +68,107 @@ module.exports = new Suite('Syntax')
     assert.ok(report.failed);
     assert.ok(!report.incomplete);
     assert.stats(report.stats, { tests: 1, failed: 1 });
-    assert.match(report.resolve(0).errors[0].message, /Timed out after 100ms/);    
+    assert.match(report.resolve(0).errors[0].message, /Timed out after 100ms/);
+  }))
+  .add(new Test('should configure after hooks with a timeout', async () => {
+    const suite = describe('Suite', () => {
+      after(async () => {
+        return new Promise((resolve) => {
+          setTimeout(resolve, 200);
+        });
+      }, { timeout: 100 });
+
+      it('Test', () => {});
+    });
+
+    const harness = new Harness(suite);
+
+    const report = await harness.run();
+    assert.ok(report.failed);
+    assert.ok(!report.incomplete);
+    assert.stats(report.stats, { tests: 1, passed: 1 });
+    assert.match(report.errors[0].message, /Timed out after 100ms/);
+  }))
+  .add(new Test('should configure afterEach hooks with a timeout', async () => {
+    const suite = describe('Suite', () => {
+      afterEach(async () => {
+        return new Promise((resolve) => {
+          setTimeout(resolve, 200);
+        });
+      }, { timeout: 100 });
+
+      it('Test', () => {});
+    });
+
+    const harness = new Harness(suite);
+
+    const report = await harness.run();
+    assert.ok(report.failed);
+    assert.ok(!report.incomplete);
+    assert.stats(report.stats, { tests: 1, failed: 1 });
+    assert.match(report.resolve(0).errors[0].message, /Timed out after 100ms/);
   }));
+
+const exclusiveSuite = new Suite('Exclusive')
+  .add(new Test('should configure exclusive suites', async () => {
+    const suite = describe('Suite 1', () => {
+      describe('Suite 2', () => {
+        it('Test 1', () => {});
+      }, { exclusive: true });
+
+      describe('Suite 3', () => {
+        it('Test 2', () => {});
+      });
+    });
+
+    const harness = new Harness(suite);
+
+    const report = await harness.run();
+    assert.ok(report.incomplete);
+    assert.stats(report.stats, { tests: 2, passed: 1 });
+  }))
+  .add(new Test('should configure exclusive tests', async () => {
+    const suite = describe('Suite', () => {
+      it('Test 1', () => {}, { exclusive: true });
+      it('Test 2', () => {});
+    });
+
+    const harness = new Harness(suite);
+
+    const report = await harness.run();
+    assert.ok(report.incomplete);
+    assert.stats(report.stats, { tests: 2, passed: 1 });
+  }))
+
+  const skippedSuite = new Suite('Skipped')
+  .add(new Test('should configure skipped suites', async () => {
+    const suite = describe('Suite 1', () => {
+      describe('Suite 2', () => {
+        it('Test 1', () => {});
+      }, { skip: true });
+
+      describe('Suite 3', () => {
+        it('Test 2', () => {});
+      });
+    });
+
+    const harness = new Harness(suite);
+
+    const report = await harness.run();
+    assert.ok(report.incomplete);
+    assert.stats(report.stats, { tests: 2, passed: 1, skipped: 1 });
+  }))
+  .add(new Test('should configure skipped tests', async () => {
+    const suite = describe('Suite', () => {
+      it('Test 1', () => {}, { skip: true });
+      it('Test 2', () => {});
+    });
+
+    const harness = new Harness(suite);
+
+    const report = await harness.run();
+    assert.ok(report.incomplete);
+    assert.stats(report.stats, { tests: 2, passed: 1, skipped: 1 });
+  }))
+
+module.exports = new Suite('Syntax').add(hooksSuite, exclusiveSuite, skippedSuite);
